0acddf7b2591767d7c532ce1b20450446407a8bf
AmanPunia-510 <puniaaman888@gmail.com>
2024-05-18 09:20:53 +0530

    section 6 complete now work on section 7

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const menu = document.querySelector(".toggle");
 
 menu.addEventListener("click", () => {
   item.classList.toggle("show");
-  let contain = document.querySelector(".all-content");
+  let contain = document.querySelector("body");
   if (item.classList.contains("show")) {
     contain.style.position = "fixed";
     contain.style.top = "0";
