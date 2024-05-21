82439fbf24f963dfcb260ab5d0efdaae882d95c8
AmanPunia-510 <puniaaman888@gmail.com>
2024-05-21 15:06:37 +0530

    project almost complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,17 @@ const menu = document.querySelector(".toggle");
 menu.addEventListener("click", () => {
   item.classList.toggle("show");
   let contain = document.querySelector("body");
+let index = document.querySelector(".js-z");
   if (item.classList.contains("show")) {
     contain.style.position = "fixed";
     contain.style.top = "0";
     contain.style.left = "0";
     contain.style.width = "100%";
+    index.classList.remove("z-2");
+
   } else {
     contain.style.position = "unset";
+    index.classList.add("z-2");
+
   }
 });
